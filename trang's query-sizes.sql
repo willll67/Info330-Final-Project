59aa81952e396a15e6c33c6f35644a87817ebf5e
htrang07 <htrang07@uw.edu>
2023-06-06 13:10:07 -0700

    updated

--- trang's query-sizes.sql
+++ trang's query-sizes.sql
@@ -3,11 +3,3 @@ FROM athlete_events
 WHERE Medal != 'NA'
 GROUP BY Medal;
 
-SELECT Season, Sex, AVG(Age) AS AverageAge
-FROM athlete_events
-GROUP BY Season, 
-SELECT Event, COUNT(*) AS ParticipantCount
-FROM athlete_events
-GROUP BY Event
-ORDER BY ParticipantCount DESC
-LIMIT 10;
